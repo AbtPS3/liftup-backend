884bde1cecd94c3039f7c306963f0db7c79a5375
Kizito Mrema <kizomanizo@gmail.com>
2024-06-21 15:32:15 +0300

    MINOR: Modified API V2 on index clients to add features.

--- services/dashboard-service.js
+++ services/dashboard-service.js
@@ -18,7 +18,50 @@ class DashboardService {
         },
       },
     });
-    return indexClients;
+
+    // Group the data by hfr_code
+    // const groupedData = indexClients.reduce((acc, client) => {
+    //   const { hfr_code } = client;
+    //   if (!acc[hfr_code]) {
+    //     acc[hfr_code] = [];
+    //   }
+    //   acc[hfr_code].push(client);
+    //   return acc;
+    // }, {});
+
+    // return groupedData;
+
+    // Group the data by hfr_code and transform it
+    const groupedData = indexClients.reduce((acc, client) => {
+      const { hfr_code, ...rest } = client;
+
+      // Remove the id field and rename keys
+      const transformedClient = {
+        ...rest,
+        registrationDate: rest.ucs_registration_date,
+        totalCTCClients: rest.ctcclients,
+        totalUCSClients: rest.ucsclients,
+        totalReachedClients: rest.reachedclients,
+        totalUnreachedClients: rest.unreachedclients,
+        totalElicitations: rest.totalelicitations,
+      };
+
+      // Delete the original keys to clean up the object
+      delete transformedClient.ucs_registration_date;
+      delete transformedClient.ctcclients;
+      delete transformedClient.ucsclients;
+      delete transformedClient.reachedclients;
+      delete transformedClient.unreachedclients;
+      delete transformedClient.totalelicitations;
+
+      if (!acc[hfr_code]) {
+        acc[hfr_code] = [];
+      }
+      acc[hfr_code].push(transformedClient);
+      return acc;
+    }, {});
+
+    return groupedData;
   }
 }
 
