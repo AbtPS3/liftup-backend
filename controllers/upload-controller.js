be2b1357f2884b556b74d2eee39bc71c23f133bc
Kizito Mrema <kizomanizo@gmail.com>
2024-08-16 23:08:22 +0300

    UPDATE: Added rejection reason for each rejected row

--- controllers/upload-controller.js
+++ controllers/upload-controller.js
@@ -2,8 +2,8 @@
  * @file upload-controller.js
  * @module controllers/upload-controller
  * @description Controller class for handling CSV file upload logic.
- * @version 1.0.1
- * @author Kizito S.M.
+ * @version 1.1.0
+ * @autor Kizito S.M.
  */
 
 import { fileURLToPath } from "url";
@@ -12,17 +12,14 @@ import csvParser from "csv-parser";
 import pkg from "csv-writer";
 import streamifier from "streamifier";
 import dotenv from "dotenv";
-dotenv.config();
-
 import CustomError from "../helpers/custom-error.js";
 import response from "../helpers/response-handler.js";
 
-// Get the current module's URL
-const currentModuleURL = new URL(import.meta.url);
-// Get the directory name
-const __dirname = dirname(fileURLToPath(currentModuleURL));
-// Destructure the csv-writer package
+dotenv.config();
+
 const { createObjectCsvWriter } = pkg;
+const currentModuleURL = new URL(import.meta.url);
+const __dirname = dirname(fileURLToPath(currentModuleURL));
 
 /**
  * Controller class for handling CSV file upload logic.
@@ -30,33 +27,19 @@ const { createObjectCsvWriter } = pkg;
  */
 class UploadController {
   /**
-   * Constructor for UploadController.
-   * @constructor
-   */
-  constructor() {
-    // Set the current directory name
-    this.__dirname = dirname(fileURLToPath(currentModuleURL));
-  }
-
-  /**
    * Handles requests to the root path.
    * @param {Object} req - Express request object.
    * @param {Object} res - Express response object.
    * @param {Function} next - Express next middleware function.
-   * @returns {Object} - JSON response containing the message and authentication status.
    */
   async all(req, res, next) {
     try {
-      // Check if the request is authenticated
-      const authenticated = req.decoded ? true : false;
-
-      // Response payload
+      const authenticated = !!req.decoded;
       const payload = {
         token: null,
-        authenticated: authenticated,
+        authenticated,
         message: "Root path reached",
       };
-
       return response.api(req, res, 200, payload);
     } catch (error) {
       console.error(error.message);
@@ -69,118 +52,97 @@ class UploadController {
    * @param {Object} req - Express request object.
    * @param {Object} res - Express response object.
    * @param {Function} next - Express next middleware function.
-   * @returns {Object} - JSON response containing the message and authentication status.
    */
   async create(req, res, next) {
     try {
-      // Check if a file is provided in the request
-      if (!req.file) {
-        throw new Error("No file provided!");
-      }
+      if (!req.file) throw new Error("No file provided!");
 
-      // Capture the original file name to determine if it's for clientscontacts
       const originalFileName = req.file.originalname;
-
-      // Fetch ctcNumbers from the provided endpoint
-      const ctcNumbersResponse = await fetch("http://localhost:8090/get-uploaded-ctc-numbers");
-      if (!ctcNumbersResponse.ok) {
-        throw new Error("Duplicate checker service unavailable. Please retry later!");
-      }
-      const ctcNumbers = await ctcNumbersResponse.json();
-
-      // Extract ctc_numbers from the response
-      const existingCtcNumbers = ctcNumbers.map((item) => item.ctc_number);
-
       const fileNameParts = originalFileName.split("_");
-      const uploadType =
-        fileNameParts[1] === "clients" ||
-        fileNameParts[1] === "contacts" ||
-        fileNameParts[1] === "results"
-          ? fileNameParts[1]
-          : null;
-
+      const uploadType = ["clients", "contacts", "results"].includes(fileNameParts[1]) ? fileNameParts[1] : null;
       const fileBuffer = req.file.buffer;
-
-      // Convert the buffer to a readable stream using streamifier
       const fileStream = streamifier.createReadStream(fileBuffer);
 
-      // Process the uploaded CSV file
+      const [ctcNumbersResponse, elicitationNumbersResponse] = await Promise.all([
+        fetch("http://localhost:8090/get-uploaded-ctc-numbers"),
+        fetch("http://localhost:8091/get-uploaded-elicitation-numbers"),
+      ]);
+
+      if (!ctcNumbersResponse.ok) throw new Error("CTC Deduplicator checker unavailable. Retry later!");
+      if (!elicitationNumbersResponse.ok) throw new Error("Elicitation Deduplicator checker unavailable. Retry later!");
+
+      const existingCtcNumbers = (await ctcNumbersResponse.json()).map((item) => item.ctc_number);
+      const existingElicitationNumbers = (await elicitationNumbersResponse.json()).map((item) => item.elicitation_number);
+
       const results = [];
       const rejectedRows = [];
       const csvStream = csvParser({ headers: true });
-
-      // Flag to check if it's the first row
       let isFirstRow = true;
+
       csvStream.on("data", (data) => {
-        // Check if ctcNumber is in existingCtcNumbers
-        const ctcNumber = data._0;
-        if (uploadType == "clients" && existingCtcNumbers.includes(ctcNumber)) {
-          rejectedRows.push(data); // If ctc_number is in existingCtcNumbers, push it to rejectedRows
+        let rejectionReason = "";
+
+        if (uploadType === "clients" && existingCtcNumbers.includes(data._0)) {
+          rejectionReason = "Duplicate CTC number in clients file";
+        } else if (["contacts", "results"].includes(uploadType) && !existingCtcNumbers.includes(data._12)) {
+          rejectionReason = uploadType === "contacts" ? "No matching index client CTC number in contacts file" : "No matching index client CTC number in results file";
+        } else if (uploadType === "results" && existingElicitationNumbers.includes(data._13)) {
+          rejectionReason = "Duplicate elicitation number in uploaded file";
+        }
+
+        if (rejectionReason) {
+          data.rejectionReason = rejectionReason;
+          rejectedRows.push(data);
         } else {
-          // Check if it's the first row
           if (isFirstRow) {
-            // Add the specified columns to the header row
             data.providerId = "providerId";
             data.team = "team";
             data.teamId = "teamId";
             data.locationId = "locationId";
-
-            // Update the flag to false for subsequent rows
             isFirstRow = false;
           } else {
-            // For every other row, obtain the data from the decoded token
             data.providerId = req.decoded.data.providerId;
             data.team = req.decoded.data.team;
             data.teamId = req.decoded.data.teamId;
             data.locationId = req.decoded.data.locationId;
           }
-
-          // Push the processed data to the results array
           results.push(data);
         }
       });
 
-      // Event handler when the CSV stream ends
       csvStream.on("end", async () => {
-        // Check if there is data available
         if (results.length > 0) {
-          // Determine the upload directory based on the uploadType
           let uploadDirectory;
 
-          if (uploadType === "clients") {
-            uploadDirectory = "index_uploads";
-          } else if (uploadType === "contacts") {
-            uploadDirectory = "contacts_uploads";
-          } else if (uploadType === "results") {
-            uploadDirectory = "results_uploads";
-          } else {
-            console.error("UploadType:", uploadType);
-            // return response.api(req, res, 500, "Upload type is null!" + req.file.originalname);
-            throw new Error(`Upload type is null! + ${req.file.originalname}`);
+          switch (uploadType) {
+            case "clients":
+              uploadDirectory = "index_uploads";
+              break;
+            case "contacts":
+              uploadDirectory = "contacts_uploads";
+              break;
+            case "results":
+              uploadDirectory = "results_uploads";
+              break;
+            default:
+              throw new Error(`Invalid upload type: ${uploadType}`);
           }
 
-          // Set the filePath based on the determined upload directory
           const filePath = join(__dirname, `../public/${uploadDirectory}`, originalFileName);
-
-          // Create a CSV writer instance
           const csvWriter = createObjectCsvWriter({
             path: filePath,
-            header: Object.keys(results[0]), // Use the keys from the first row as headers
-            alwaysQuote: true, // Ensure all values are quoted
+            header: Object.keys(results[0]),
+            alwaysQuote: true,
           });
 
-          // Write records to the CSV file
           await csvWriter.writeRecords(results);
 
-          const rejected = rejectedRows.length > 0 ? true : false;
-
-          // Response payload
           const payload = {
             token: null,
             authenticated: true,
             message: "File uploaded, processed, and saved successfully!",
-            rejected: rejected,
-            rejectedRows: rejectedRows,
+            rejected: rejectedRows.length > 0,
+            rejectedRows,
           };
 
           return response.api(req, res, 201, payload);
@@ -189,7 +151,6 @@ class UploadController {
         }
       });
 
-      // Process the uploaded CSV file using the readable stream
       fileStream.pipe(csvStream);
     } catch (error) {
       console.error(error.message);
