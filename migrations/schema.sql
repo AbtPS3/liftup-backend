c6d26ed20e889bbfc5ab100c41b0a13f9cb2eac2
Kizito Mrema <kizomanizo@gmail.com>
2024-08-30 17:09:10 +0300

    MINOR: Updated migrations

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA tepi_stats
-    AUTHORIZATION postgres;
-
+CREATE SCHEMA tepi_stats AUTHORIZATION postgres;
 COMMENT ON SCHEMA tepi_stats
     IS 'For storing personal file upload stats for TEPI initiative.';
